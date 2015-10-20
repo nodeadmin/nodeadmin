473d0c33a1fbb8c0c606147c8539369e275ca257
Laura Knight <ljknight@gmail.com>
2015-10-20 16:54:36 -0700

    Create React structure for setup and login pages

---
+++ middleware/public/src/navbar/navbar.js
@@ -0,0 +1,34 @@
+// var Tabs = require('material-ui/lib/tabs/tabs');
+// var Tab = require('material-ui/lib/tabs/tab');
+
+// var NavBar = React.createClass({
+  // constructor: function () {
+  //   super();
+  //   this._handleRoute = this._handleRoute.bind(this);
+  // },
+  // _handleRoute: function (e) {
+  //   e.preventDefault();
+  //   console.log('route handled');
+  //   Router.routes['setup']();
+  // },
+  // render: function () {
+      // onActive={Router.routes['home']()}
+      // <Tabs>
+      //   <Tab label="Home"></Tab>
+      //   <Tab label="Database" ></Tab>
+      //   <Tab label="System" ></Tab>
+      //   <Tab label="Settings" ></Tab>
+    //   // </Tabs>
+    // return (
+    //   <ul>
+    //     <li>Home</li>
+    //     <li>Database</li>
+    //     <li>System</li>
+    //     <li>Settings</li>
+    //     <li>Log Out</li>
+      // </ul>
+    // )
+//   }
+// });
+
+// module.exports = NavBar;
