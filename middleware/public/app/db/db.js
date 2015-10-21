bf530767c1b6363f9d2c8672279b6dcdb483f602
Andrew Nickell <nickell.andrew@gmail.com>
2015-10-21 15:57:08 -0700

    adds sidebar to db

--- middleware/public/app/db/db.js
+++ middleware/public/app/db/db.js
@@ -1,4 +1,8 @@
 angular.module('nodeadmin.db', [])
 .controller('DBController', ['$scope', function ($scope) {
   $scope.awesomeTable = 'I am an Awesome Table';
+  $scope.toggleMenu = function () {
+    $scope.menu = !$scope.menu;
+  }
+  $scope.menu = false;
 }]);
