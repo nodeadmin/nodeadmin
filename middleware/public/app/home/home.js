647bdfe349024c04a308e6d07bfacfd9214dbbf6
taylorleh <taylorleh@mac.com>
2015-10-21 21:57:52 -0700

    removes extra file size filter

--- middleware/public/app/home/home.js
+++ middleware/public/app/home/home.js
@@ -37,12 +37,6 @@ angular.module('nodeadmin.home', [])
     return toSize(bytes);
   }
 
-  var bytesToSize = function(bytes) {
-     var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-     if (bytes == 0) return '0 Byte';
-     var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-     return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-  };
 
   $scope.getServerStats = function() {
     Stats.serverStats()
