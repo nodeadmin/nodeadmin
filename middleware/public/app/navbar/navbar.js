6be057a7108465e66abf4e123d5f9d9bdd760a0f
Andrew Nickell <nickell.andrew@gmail.com>
2015-10-21 19:33:18 -0700

    navbar now switches

--- middleware/public/app/navbar/navbar.js
+++ middleware/public/app/navbar/navbar.js
@@ -5,38 +5,7 @@ angular.module('nodeadmin.navbar', [])
     $state.transitionTo('login');
   };
 
-  $scope.homeActive = true;
-  $scope.dBActive = false;
-  $scope.systemActive = false;
-  $scope.settingsActive = false;
-  $scope.activeTab = 'Home';
-
-  $scope.setActiveTab = function (tabName) {
-    console.log(tabName);
-    $scope.activeTab = tabName;
-    if (tabName === 'Home') {
-      $scope.homeActive = true;
-    } else {
-      $scope.homeActive = false;
-    }
-
-    if (tabName === 'DB') {
-      $scope.dBActive = true;
-    } else {
-      $scope.dBActive = false;
-    }
-
-    if (tabName === 'System') {
-      $scope.systemActive = true;
-    } else {
-      $scope.systemActive = false;
-    }
-
-    if (tabName === 'Settings') {
-      $scope.settingsActive = true;
-    } else {
-      $scope.settingsActive = false;
-    }
-    // console.log('Home:',$scope.homeActive,' DB:',$scope.dBActive,' System:',$scope.systemActive,' Settings:',$scope.settingsActive);
-  }
+  var state = $state.current.name;
+  $scope._state = state;
+  
 }]);
