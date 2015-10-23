5ac6d5307af7adff54dd067c30769ab150b9bac0
Andrew Nickell <nickell.andrew@gmail.com>
2015-10-23 16:45:48 -0700

    added menus to settings/system

--- middleware/public/app/settings/settings.js
+++ middleware/public/app/settings/settings.js
@@ -1,4 +1,9 @@
 angular.module('nodeadmin.settings', [])
 .controller('SettingsController', ['$scope', function ($scope) {
   $scope.somesettings = 'I am some settings and stuff';
+
+  $scope.toggleMenu = function () {
+    $scope.menu = !$scope.menu;
+  }
+  $scope.menu = false;
 }]);
--- middleware/public/app/system/system.js
+++ middleware/public/app/system/system.js
@@ -1,4 +1,9 @@
 angular.module('nodeadmin.system', [])
 .controller('SystemController', ['$scope', function ($scope) {
   $scope.system = 'system stuff';
+
+  $scope.toggleMenu = function () {
+    $scope.menu = !$scope.menu;
+  }
+  $scope.menu = false;
 }]);
