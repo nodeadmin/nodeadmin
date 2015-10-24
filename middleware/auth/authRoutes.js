fe947c86b2875cf43aaf57a7211db21778ec8283
Alex Hutchison <ahutch006@gmail.com>
2015-10-24 16:29:06 -0700

    switch to using mysql info exclusively

--- middleware/auth/authRoutes.js
+++ middleware/auth/authRoutes.js
@@ -1,22 +1,20 @@
 var express = require('express');
 var router = express.Router();
-// var Promise = require("bluebird");
 var mysql = require('promise-mysql');
-var authCtrl = require('./authCtrl');
+var jwt = require('jsonwebtoken');
 var connection;
-router.route('/setup')
+router.route('/login')
   .get(function (req, res) {
     res.send('eyyyy in auth');
   })
   .post(function (req, res) {
     mysql.createConnection({
-      host: req.body.host,
       user: req.body.mysqlUser,
       password: req.body.mysqlPassword
     }).then(function(conn) {
         connection = conn;
-        var obj = authCtrl.authCtrl(conn);
-        obj(req, res);
+        var token = jwt.sign({msg: 'welcome!'}, req.app.locals.secret);
+				res.status(200).json({token: token});
     }).catch(function (e) {
       console.log(e);
       if(e.errno === 1045) {
@@ -28,25 +26,5 @@ router.route('/setup')
       }
     });
   });
-router.route('/dbcheck')
-  .get(function(req, res) {
-    connection.query('SHOW DATABASES')
-      .then(function (results) {
-        results.forEach(function (result) {
-          if (result.Database === 'nodeAdmin') {
-            res.send(true);
-          }
-        });
-        res.send(false);
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-  });
-router.route('/login')
-.post(function (req, res) {
-  var user = req.body.username;
-  var pass = req.body.pass;
 
-})
 module.exports = router;
