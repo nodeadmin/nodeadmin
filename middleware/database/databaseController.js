8eea44c90db31c589941377c30ced60c8769d52a
Andrew Nickell <nickell.andrew@gmail.com>
2015-10-26 14:41:25 -0700

    enables performance_schema on db connection

--- middleware/database/databaseController.js
+++ middleware/database/databaseController.js
@@ -1,12 +1,7 @@
 var  mysql = require('mysql');
 var client = require('../auth/clientdb.js');
 
-db.query(
-  "update performance_schema.setup_consumers set enabled='YES' where name='events_waits_current';", 
-  function (err, rows, fields) {
-    console.log('enabling performance_schema... err: ', err, 'rows: ', rows);
-  }
-);
+
 
 module.exports = {
 
@@ -15,8 +10,8 @@ module.exports = {
   },
 
   connect: function (req, res) {
-    client = client.getClientDB();
-    client.query('SHOW DATABASES', function(err, row) {
+    db = client.getClientDB();
+    db.query('SHOW DATABASES', function (err, row) {
       row && res.end(JSON.stringify(row));
     });
   },
