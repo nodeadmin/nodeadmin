493a084cf59ba125c681f8c93bcf79a0a1521419
Andrew Nickell <nickell.andrew@gmail.com>
2015-10-26 12:21:07 -0700

    enable performance schema, fix typos/format, rename default db route

--- middleware/database/databaseController.js
+++ middleware/database/databaseController.js
@@ -1,6 +1,12 @@
 var  mysql = require('mysql');
 var client = require('../auth/clientdb.js');
 
+db.query(
+  "update performance_schema.setup_consumers set enabled='YES' where name='events_waits_current';", 
+  function (err, rows, fields) {
+    console.log('enabling performance_schema... err: ', err, 'rows: ', rows);
+  }
+);
 
 module.exports = {
 
