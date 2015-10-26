2241980fed5e73a2d57d0d6bf11b4ab774379672
Laura Knight <ljknight@gmail.com>
2015-10-26 16:44:00 -0700

    Clean up services.js

--- middleware/public/app/services/services.js
+++ middleware/public/app/services/services.js
@@ -1,28 +1,30 @@
 angular.module('nodeadmin.services', [])
 
-.factory('Auth', ['$http', '$window', function($http, $window) {
-  var login = function(user) {
-    return $http({
-      method: 'POST',
-      url: '/nodeadmin/api/auth/login',
-      data: user
-    }).then(function(resp) {
-      return resp.data.token;
-    });
-  };
+.factory('Auth', ['$http', '$window',
+  function($http, $window) {
+    var login = function(user) {
+      return $http({
+        method: 'POST',
+        url: '/nodeadmin/api/auth/login',
+        data: user
+      }).then(function(resp) {
+        return resp.data.token;
+      });
+    };
 
-  var isAuth = function() {
-    return !!$window.localStorage.getItem('nodeadmin');
-  };
+    var isAuth = function() {
+      return !!$window.localStorage.getItem('nodeadmin');
+    };
 
-  return {
-    login: login,
-    isAuth: isAuth,
-  };
+    return {
+      login: login,
+      isAuth: isAuth,
+    };
 
-}])
+  }
+])
 
-.factory('System', function ($http) {
+.factory('System', function($http) {
   var getModules = function() {
     return $http({
       method: 'GET',
@@ -37,51 +39,54 @@ angular.module('nodeadmin.services', [])
   };
 })
 
-.factory('Stats', function ($http) {
+.factory('Stats', function($http) {
   return {
-    serverStats:function() {
+    serverStats: function() {
       return $http({
-        method:'GET',
-        url:'/nodeadmin/api/home/os'
-      })
-      .then(function (data) {
-        return data;
-      })
-      .catch(function (err) {
-        return err
-      });
+          method: 'GET',
+          url: '/nodeadmin/api/home/os'
+        })
+        .then(function(data) {
+          return data;
+        })
+        .catch(function(err) {
+          return err
+        });
     }
   };
 
 })
 
-.factory('RecordsFactory', ['$http', function ($http) {
-  return {
-    getRecords: function (db, table) {
-      return $http.get('/nodeadmin/api/db/' + db + '/' + table + '/records')
-      .then(function (response) {
-        console.log(response.data);
-        return response.data;
-      })
-      .catch(function (err) {
-        return err;
-      })
-    }
-  };
-
-  .factory('TablesFactory', ['$http', function ($http) {
+.factory('RecordsFactory', ['$http',
+  function($http) {
     return {
-      getRecords: function (db) {
+      getRecords: function(db, table) {
+        return $http.get('/nodeadmin/api/db/' + db + '/' + table + '/records')
+          .then(function(response) {
+            console.log(response.data);
+            return response.data;
+          })
+          .catch(function(err) {
+            return err;
+          })
+      }
+    }
+  }
+])
+
+.factory('TablesFactory', ['$http',
+  function($http) {
+    return {
+      getRecords: function(db) {
         return $http.get('/nodeadmin/api/db/' + db + '')
-        .then(function (response) {
-          console.log(response.data);
-          return response.data;
-        })
-        .catch(function (err) {
-          return err;
-        })
+          .then(function(response) {
+            console.log(response.data);
+            return response.data;
+          })
+          .catch(function(err) {
+            return err;
+          })
       }
     };
-}]);
-  
-
+  }
+]);
