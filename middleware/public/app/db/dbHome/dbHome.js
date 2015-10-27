cf5e2b8772708aaf0006f396a37926d14cfea280
Andrew Nickell <nickell.andrew@gmail.com>
2015-10-26 17:08:39 -0700

    Shows performance_timers data (all null in our wimpy server)

--- middleware/public/app/db/dbHome/dbHome.js
+++ middleware/public/app/db/dbHome/dbHome.js
@@ -1,4 +1,12 @@
 angular.module('nodeadmin.db.dbhome', [])
-.controller('DBHomeController', ['$scope', function ($scope) {
-
-}])
+.controller('DBHomeController', ['$scope', 'PerformanceGraphFactory', 
+  function ($scope, PerformanceGraphFactory) {
+    var perfData;
+    PerformanceGraphFactory.getPerformanceTimers()
+    .then(function(data) {
+      var perfData = data;
+      $scope.headers = Object.keys(perfData[0])
+      $scope.rows = perfData;
+    });
+  }
+]);
