a18256a8402ba53d9e0c9cf57e3a2de525885d4b
Alex Hutchison <ahutch006@gmail.com>
2015-10-27 15:49:24 -0700

    table cells are now editable

--- middleware/auth/authRoutes.js
+++ middleware/auth/authRoutes.js
@@ -10,7 +10,8 @@ router.route('/login')
   .post(function (req, res) {
     mysql.createConnection({
       user: req.body.mysqlUser,
-      password: req.body.mysqlPassword
+      password: req.body.mysqlPassword,
+      multipleStatements: true 
     }).then(function(conn) {
         var token = jwt.sign({msg: 'welcome!'}, req.app.locals.secret);
         //Enables performance_schema if it was disabled\\
