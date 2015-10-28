7ed07426a7ddf7c135eb9d02b4745ef8e51f8743
Andrew Nickell <nickell.andrew@gmail.com>
2015-10-28 16:01:10 -0700

    close all toggle-menu tags O.o

--- middleware/public/app/directives/togglemenu.js
+++ middleware/public/app/directives/togglemenu.js
@@ -1,11 +1,6 @@
 angular.module('nodeadmin.togglemenu', [])
 .directive('toggleMenu', function () {
-  return { template: 
-    '<div class="menu-div" style="background-color: black;">' +
-      '<a ng-click="toggleMenu()" class="btn btn-default btn-lg toggle-menu" id="menu-toggle">' +
-        '<span class="glyphicon" ng-class=\'{"glyphicon-chevron-right": menu, "glyphicon-chevron-left": !menu}\' aria-hidden="true">' +
-        '</span>' +
-      '</a>' +
-    '</div>'
+  return { 
+    templateUrl: 'app/directives/togglemenu.html'
   }
-})
+});
