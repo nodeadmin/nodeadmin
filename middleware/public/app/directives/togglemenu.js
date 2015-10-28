7d191da8e2cca71b66ff5237b2346dcb35b201d0
Alex Hutchison <ahutch006@gmail.com>
2015-10-28 15:38:57 -0700

    add loading flag

--- middleware/public/app/directives/togglemenu.js
+++ middleware/public/app/directives/togglemenu.js
@@ -1,11 +1,11 @@
 angular.module('nodeadmin.togglemenu', [])
 .directive('toggleMenu', function () {
-  return { template: 
+  return { template:
     '<div class="menu-div" style="background-color: black;">' +
       '<a ng-click="toggleMenu()" class="btn btn-default btn-lg toggle-menu" id="menu-toggle">' +
         '<span class="glyphicon" ng-class=\'{"glyphicon-chevron-right": menu, "glyphicon-chevron-left": !menu}\' aria-hidden="true">' +
         '</span>' +
       '</a>' +
     '</div>'
-  }
-})
+  };
+});
