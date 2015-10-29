1f7fe979bd113fe6d452cc2b2e7488a8d9166d7d
Laura Knight <ljknight@gmail.com>
2015-10-29 12:27:07 -0700

    Users page displays MySQL user table

--- middleware/public/app/settings/users/users.js
+++ middleware/public/app/settings/users/users.js
@@ -1,15 +1,28 @@
 angular.module('nodeadmin.settings.users', [])
   .controller('UsersController', ['$scope', 'Users',
     function($scope, Users) {
+      $scope.primaryKey = [];
+
       $scope.getUsers = function() {
         Users.getUsers()
           .then(function(result) {
             console.log('result', result)
+            $scope.users = result[0];
+            $scope.headers = result[1];
+            $scope.getPrimaryKey($scope.headers);
           })
           .catch(function(err) {
             $scope.error = err.data;
-          })
+          });
       };
       $scope.getUsers();
+
+      $scope.getPrimaryKey = function(headers) {
+        for (var i = 0; i < headers.length; i++) {
+          if (headers[i].Key === 'PRI') {
+            $scope.primaryKey.push(headers[i].Field);
+          }
+        }
+      };
     }
   ]);
