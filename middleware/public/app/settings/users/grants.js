5d490678d59e09c145871acd4dfc9d9f4f8a810a
Laura Knight <ljknight@gmail.com>
2015-10-29 17:05:21 -0700

    Set up routes and files for viewing user grants

---
+++ middleware/public/app/settings/users/grants.js
@@ -0,0 +1,6 @@
+angular.module('nodeadmin.settings.grants', [])
+  .controller('GrantsController', function($scope, Users) {
+    
+    // need user and host from page
+    // Users.getGrants(user, host)
+});
