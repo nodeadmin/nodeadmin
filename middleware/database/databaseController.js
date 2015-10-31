24f589d306e1cf59be7e0aded0a3ac9608c2ae33
taylorleh <taylorleh@mac.com>
2015-10-31 00:51:36 -0700

    update status codes

--- middleware/database/databaseController.js
+++ middleware/database/databaseController.js
@@ -157,7 +157,7 @@ module.exports = {
       if(err) {
         res.status(400).json(err);
       } else {
-        res.status(200).json(result);
+        res.status(201).json(result);
       }
     })
   },
