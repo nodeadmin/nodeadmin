0f3a84f1b05cda40d8507f84cdeb72b679e5fa4d
Andrew Nickell <nickell.andrew@gmail.com>
2015-11-03 19:42:38 -0800

    remove controller in alertcenter directive

--- middleware/public/app/directives/alertcenter.js
+++ middleware/public/app/directives/alertcenter.js
@@ -1,14 +1,4 @@
 angular.module('nodeadmin.alertcenter', [])
-.controller('AlertCenterController', ['$scope', function ($scope) {
-
-  $scope.alerts = '='; //binds to passed in object
-  //alerts must have a msg and a status property
-
-    $scope.closeAlert = function(type, index) {
-      $scope.alerts[type].splice(index, 1);
-    };
-
-}])
 .directive('alertCenter', function () {
   return { 
     templateUrl: 'app/directives/alertcenter.html'
