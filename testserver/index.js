3d3137dded59517988e4d5787c4dd4bc566d5708
Taylor Lehman <taylorlehman@Taylors-MacBook-Pro.local>
2016-02-24 23:09:30 -0800

    adds heroku production code

--- testserver/index.js
+++ testserver/index.js
@@ -5,13 +5,13 @@ var mysql = require('mysql');
 var nodeadmin = require(__dirname + '/../middleware/index.js');
 
 
-app.use(nodeadmin(app, 4040));
+app.use(nodeadmin(app, process.env.PORT || 4040));
 app.use('/', function(req, res, next) {
 
   res.send('<h1>HELLO WORLD</h1>');
 
 });
 
-app.listen(4040);
+app.listen(process.env.PORT || 4040);
 
 module.exports = app;
