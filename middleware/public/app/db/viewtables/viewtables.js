e53566457625de575242bb2551e00bdd4a52501f
Taylor Lehman <taylorlehman111@gmail.com>
2016-05-04 00:10:59 -0700

    (fix) fixes tables unable to load if db name is uppercase

--- middleware/public/app/db/viewtables/viewtables.js
+++ middleware/public/app/db/viewtables/viewtables.js
@@ -1,68 +1,68 @@
 angular.module('nodeadmin.db.viewTables', [])
-  .controller('TableViewController', ['$scope', '$uibModal', '$stateParams', 'Tables', 'AlertCenter',
-    function($scope, $uibModal, $stateParams, Tables, AlertCenter) {
+.controller('TableViewController', ['$scope', '$uibModal', '$stateParams', 'Tables', 'AlertCenter',
+  function ($scope, $uibModal, $stateParams, Tables, AlertCenter) {
 
-      $scope.tables = [];
+    $scope.tables = [];
 
-      AlertCenter.addAll($scope);
+    AlertCenter.addAll($scope);
 
-      $scope.getTables = function() {
-        var databaseName = $stateParams.database;
-        $scope.databaseName = databaseName;
-        Tables.getTables(databaseName)
-          .then(function(result) {
-            result.forEach(function(table) {
-              $scope.tables.push(table['Tables_in_' + databaseName]);
-            });
-          })
-          .catch(function(err) {
-            $scope.error = err.data;
-            $scope.alerts.error.push({
-              status: 'Error',
-              msg: err.data
-            });
+    $scope.getTables = function () {
+      var databaseName = $stateParams.database;
+      $scope.databaseName = databaseName;
+      Tables.getTables(databaseName)
+      .then(function (result) {
+        result.forEach(function (table) {
+          $scope.tables.push(table['Tables_in_' + databaseName.toLocaleLowerCase()]);
+        });
+      })
+      .catch(function (err) {
+        $scope.error = err.data;
+        $scope.alerts.error.push({
+          status: 'Error',
+          msg: err.data
+        });
+      });
+    };
+
+    // Get tables on load
+    $scope.getTables();
+
+    // `Delete table` modal
+    $scope.animationsEnabled = true;
+
+    $scope.open = function (size, tableName) {
+      $scope.tableName = tableName;
+      Tables.saveTableName(tableName);
+
+      var modalInstance = $uibModal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'app/db/viewtables/deletetable.html',
+        controller: 'DeleteTableController',
+        size: size
+      });
+
+      modalInstance.result.then(function (result) {
+        if (result === $scope.tableName) {
+          // Reload current tables in view
+          $scope.tables = [];
+          $scope.getTables();
+          $scope.success = 'Successfully deleted ' + result;
+          $scope.alerts.success.push({
+            msg: 'Successfully deleted ' + result,
+            status: '200'
           });
-      };
+        } else {
+          $scope.error = result.data;
+          $scope.alerts.error.push({
+            msg: result.data,
+            status: '400'
+          });
+        }
+      });
+    };
 
-      // Get tables on load
-      $scope.getTables();
-
-      // `Delete table` modal
-      $scope.animationsEnabled = true;
-
-      $scope.open = function(size, tableName) {
-        $scope.tableName = tableName;
-        Tables.saveTableName(tableName);
-
-        var modalInstance = $uibModal.open({
-          animation: $scope.animationsEnabled,
-          templateUrl: 'app/db/viewtables/deletetable.html',
-          controller: 'DeleteTableController',
-          size: size
-        });
-
-        modalInstance.result.then(function(result) {
-          if (result === $scope.tableName) {
-            // Reload current tables in view
-            $scope.tables = [];
-            $scope.getTables();
-            $scope.success = 'Successfully deleted ' + result;
-            $scope.alerts.success.push({
-              msg: 'Successfully deleted ' + result,
-              status: '200'
-            });
-          } else {
-            $scope.error = result.data;
-            $scope.alerts.error.push({
-              msg: result.data,
-              status: '400'
-            });
-          }
-        });
-      };
-
-      $scope.toggleAnimation = function() {
-        $scope.animationsEnabled = !$scope.animationsEnabled;
-      };
-    }
-  ]);
+    $scope.toggleAnimation = function () {
+      $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
+  }
+]);
